351c9e3a75b7424931b99be3ff978b942de60b2b
biraSRT <95512405+biraSRT@users.noreply.github.com>
2022-05-07 21:46:53 -0400

    second commit

---
+++ server/index.js
@@ -0,0 +1,40 @@
+
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const bodyParser = require('body-parser');
+
+const {
+   
+
+} = require("./handlers");
+
+const PORT = 3000;
+const app = express()
+
+
+app.use(morgan("tiny"))
+app.use(helmet());
+app.use(cors());
+app.use(bodyParser.json());
+
+//endpoints//
+
+
+
+
+//catch all
+app.get("*", (req, res) => {
+    res.status(400).json({
+    message: "Tags parameter is required",
+    status: 400,
+    
+    });
+})
+
+
+//endpoints//
+
+app.listen(PORT, () => console.log(`Listening on Port ${PORT}`));
+
